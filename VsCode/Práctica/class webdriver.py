8cf2a16936eeaf99dc649a1dca8240406aa5be23
dquinonessanchez <david.quinonessanchez@outpayce.com>
2025-05-06 13:30:00 +0200

    eliminado de archivos erroneos

--- VsCode/Práctica/class webdriver.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-cService = webdriver.ChromeService(executable_path='C:\Program Files (x86)\chromedriver.exe')
-driver = webdriver.Chrome(service = cService)
-
-driver.get("https://techwithtim.net")
-print(driver.title)
